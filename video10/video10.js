8f5b5c2a18001218af8150311e7d4f1a6c679f4a
Minh Quan <minhquanthcs257@gmail.com>
2025-02-23 22:51:07 +0700

    Loop

---
+++ video10/video10.js
@@ -0,0 +1,21 @@
+// Vòng lặp for
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+// Vòng lặp while
+let a = 4;
+while (a < 10) {
+  console.log("Hi");
+  a++;
+}
+//Vòng lặp do...while
+let b = 8;
+do {
+  console.log("love");
+  b++;
+} while (b < 10);
+// loop với arr
+let arr = ["Tai", "Quan", "Hoan", "Hon"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
